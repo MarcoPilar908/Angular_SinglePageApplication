9660804d89a058a09133983e9b3bc5fd5b2defe7
MarcoPilar <marcopilar908@gmail.com>
2021-09-23 13:21:07 -0500

    Estableciendo interface y agregando datos

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+heroes : Heroe [] = [];
 
-  constructor() { }
+  constructor(private _heroesService : HeroesService) { }
 
   ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
   }
 
 }
