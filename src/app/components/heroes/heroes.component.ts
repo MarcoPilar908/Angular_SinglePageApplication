a5061930cd9b7427ce40a77354db0352bd175f10
MarcoPilar <marcopilar908@gmail.com>
2021-09-23 15:30:39 -0500

    Rutas con parámetros del módulo

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
@@ -7,10 +8,15 @@ import { HeroesService, Heroe } from '../../servicios/heroes.service';
 export class HeroesComponent implements OnInit {
 heroes : Heroe [] = [];
 
-  constructor(private _heroesService : HeroesService) { }
+  constructor(private _heroesService : HeroesService, private _router: Router) { 
+    // console.log("constructor");
+  }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
+    // console.log(this.heroes);
   }
-
+  verHeroe(idx: number){
+    this._router.navigate(['/heroe',idx]);
+    }
 }
