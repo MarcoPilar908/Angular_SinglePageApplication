0316a50f073091553bba99b5db45e934d8cf9876
MarcoPilar <marcopilar908@gmail.com>
2021-09-29 13:49:39 -0500

    Recibiendo parámetros por URL

--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -59,6 +59,9 @@ export class HeroesService {
     getHeroes() : Heroe[] {
         return this.heroes;
     }
+    getHeroe(idx:string){
+      return this.heroes[idx];
+    }
 
 }
 export interface Heroe{
